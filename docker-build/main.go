9431d7b707d2c9711fd1dd9572c39878249d4c4c
Enrique Encalada <encalada@de.ibm.com>
2021-06-17 09:14:27 +0200

    buggy

--- docker-build/main.go
+++ docker-build/main.go
@@ -12,7 +12,7 @@ import (
 
 func main() {
 	port := 8080
-	if strValue, ok := os.LookupEnv("PORT"); ok {
+	if strValue, ok := os.LookupEnv("PORT"); ok 
 		if intValue, err := strconv.Atoi(strValue); err == nil {
 			port = intValue
 		}
